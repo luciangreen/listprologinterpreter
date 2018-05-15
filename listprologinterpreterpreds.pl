17c9f825165c6c331f99e6b5f7d4dd19f886ab85
luciangreen <luciangreen@lucianacademy.com>
2018-05-15 20:55:55 +1000

    Update listprologinterpreterpreds.pl

--- listprologinterpreterpreds.pl
+++ listprologinterpreterpreds.pl
@@ -1,11 +1,13 @@
 interpretpart(is,Variable1,Value1,Vars1,Vars2) :-
-        getvalue(Variable1,Value1A,Vars1),
-	isvalstr(Value1),
-        val1emptyorvalsequal(Value1A,Value1),
+        getvalue(Value1,Value1A,Vars1),
+	%%isvalstr(Value1),
+	%%isvalstr(Value1A),
+	expression(Value1A),
+        %%val1emptyorvalsequal(Value1A,Value1),
 	%%isval(Value2),
-        putvalue(Variable1,Value1,Vars1,Vars2),
-		(debug(on)->(writeln([call,[variable,is,Value1],"Press c."]),(not(get_single_char(97))->true;abort));true),
-		(debug(on)->(writeln([exit,[Value1,is,Value1],"Press c."]),(not(get_single_char(97))->true;abort));true).
+        putvalue(Variable1,Value1A,Vars1,Vars2),
+		(debug(on)->(writeln([call,[variable,is,Value1A],"Press c."]),(not(get_single_char(97))->true;abort));true),
+		(debug(on)->(writeln([exit,[Variable1,is,Value1A],"Press c."]),(not(get_single_char(97))->true;abort));true).
 interpretpart(isplus,Variable1,Variable2,Variable3,Vars1,Vars2) :-
         getvalues(Variable1,Variable2,Variable3,Value1,Value2,Value3,Vars1),
         Value1A is Value2 + Value3,
@@ -68,6 +70,16 @@ interpretpart(member,Variable1,Variable2,Vars1,Vars2) :-
 	putvalue(Variable1,Value3,Vars1,Vars2),
                 (debug(on)->(writeln([call,[member,variable1,Value2],"Press c."]),(not(get_single_char(97))->true;abort));true),
                 (debug(on)->(writeln([exit,[member,Value3,Value2],"Press c."]),(not(get_single_char(97))->true;abort));true))).
+
+interpretpart(delete,Variable1,Variable2,Variable3,Vars1,Vars2) :-
+        getvalues(Variable1,Variable2,Variable3,Value1,Value2,Value3,Vars1),
+        delete(Value1,Value2,Value3A),
+        val1emptyorvalsequal(Value3,Value3A),
+        putvalue(Variable3,Value3A,Vars1,Vars2),
+        	(debug(on)->(writeln([call,[delete,Value1,Value2,variable3],"Press c."]),(not(get_single_char(97))->true;abort));true),
+        	(debug(on)->(writeln([exit,[delete,Value1,Value2,Value3A],"Press c."]),(not(get_single_char(97))->true;abort));true).
+
+
 interpretpart(append,Variable1,Variable2,Variable3,Vars1,Vars2) :-
         getvalues(Variable1,Variable2,Variable3,Value1,Value2,Value3,Vars1),
         append1(Value1,Value2,Value3A),
@@ -93,7 +105,14 @@ append1([],Item,Item) :-
 append1(Item1,Item2,Item3) :-
 	((isvalstr(Item1),Item1A=[Item1]);(not(isvalstr(Item1)),Item1A=Item1)),
         ((isvalstr(Item2),Item2A=[Item2]);(not(isvalstr(Item2)),Item2A=Item2)),
-        ((isvalstr(Item3),Item3A=[Item3]);(not(isvalstr(Item3)),Item3A=Item3)),
-	append(Item1A,Item2A,Item3A).
+        %%((isvalstr(Item3),Item3A=[Item3]);(not(isvalstr(Item3)),Item3A=Item3)),
+	append(Item1A,Item2A,Item3).
+/**delete1(Item1,Item2,Item3) :-
+	((isvalstr(Item1),Item1A=[Item1]);(not(isvalstr(Item1)),Item1A=Item1)),
+        ((isvalstr(Item2),Item2A=[Item2]);(not(isvalstr(Item2)),Item2A=Item2)),
+        %%((isvalstr(Item3),Item3A=[Item3]);(not(isvalstr(Item3)),Item3A=Item3)),
+	delete(Item1A,Item2A,Item3).
+**/
+
 removebrackets([[Value]],Value) :-!.
 removebrackets(Value,Value).
