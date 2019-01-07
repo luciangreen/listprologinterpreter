95d4f16ef7f83294fd426d9ecb38463009148419
Lucian Green <luciangreen@lucianacademy.com>
2019-01-08 01:56:23 +1100

    Add files via upload

---
+++ listprolog.pl
@@ -0,0 +1,6 @@
+:- include('grammar.pl').
+:- include('listprologinterpreter1listrecursion4.pl').
+:- include('listprologinterpreter3preds5.pl').
+:- include('lpiverify4.pl').
+:- include('rcaw/rcaw.pl').
+:- include('rcaw/texttobr2.pl').
