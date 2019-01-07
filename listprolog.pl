834d6b50b8475e88e35c81e391a1f6b0e1c4959f
Lucian Green <luciangreen@lucianacademy.com>
2019-01-08 02:00:05 +1100

    Update listprolog.pl

--- listprolog.pl
+++ listprolog.pl
@@ -2,5 +2,3 @@
 :- include('listprologinterpreter1listrecursion4.pl').
 :- include('listprologinterpreter3preds5.pl').
 :- include('lpiverify4.pl').
-:- include('rcaw/rcaw.pl').
-:- include('rcaw/texttobr2.pl').
