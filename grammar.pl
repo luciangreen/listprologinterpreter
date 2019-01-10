a00c4602e61f102d5bb38f8a13817f7cd3e272e2
Lucian Green <luciangreen@lucianacademy.com>
2019-01-10 20:23:10 +1100

    Update grammar.pl

--- grammar.pl
+++ grammar.pl
@@ -1,3 +1,10 @@
+%% Replace with grammar part, write grammar part, write grammars to convert string to compound
+
+%% Replace with grammar part
+
+%% Make atoms into strings as soon as entered x have both, because it is annoying to enter strings as strings* - prefer atoms because they're faster, use strings only when necessary - * get rid of atoms as strings and use atoms only as names in form [n,name], v
+
+%% ** Need compound terms in LPI
 %% convert_to_grammar_part1([[n,nouns],[[v,l1],[v,l2]]],"->",[[[n,word],[[v,noun1]]],[[n,word],[[v,noun2]]],[[n,code],[[n,append],[[v,l1],[[v,noun1],[v,noun2]],[v,l2]]]]]]],[],G).
 %% convert_to_grammar_part1([[[n,noun],[[v,l1],[v,l2]],"->",[[[n,word],[[v,noun1]]]]]],[],G).
 %% convert_to_grammar_part1([[[n,noun],"->",[[[n,word],[[v,noun1]]]]]],[],G).
@@ -6,6 +13,38 @@
 %% convert_to_grammar_part1([[[n,noun],"->",[["apple"]]]],[],G).
 %% convert_to_grammar_part1([[[n,noun],"->",["apple"]]],[],G).
 
+%% Notes
+%% Brackets around body in input v
+
+%% base case [] v
+%% calls, terminals, code v
+%% if calls - 2vp or non 2vp v
+
+%% No bc needed except [] v
+%% Recurse after all except bc [] v
+
+%% code in converted code has no brackets v
+
+%% $->v v
+
+%% must have a call or terminal v
+
+%% terminals have vgps, undef g t->g p v
+
+%% can find x as terminals, can find singletons later v
+
+%% grammar_part,[n,noun],[v,vgp1],[v,vgp2] should be
+%% grammar_part,[[n,noun],[v,vgp1],[v,vgp2]] do using list proc v
+
+%% append [[]] not [] to [[v v]] v
+
+%% Print [] around body v
+
+%% append list in first query args 1<->2 v
+
+%% ** change [v,name] to [v*,name] so new interpreter, grammar command can run them, can have multiple v, v* or groups of these used in an interpreter shell x use a marker to avoid replacing variables with values when passing algorithms to shells, or detect and leave without marker
+
+%% keep original length recorded to add base case in case when add variables
 convert_to_grammar_part1([],Grammar,Grammar) :- !.
 convert_to_grammar_part1(Grammar1,Grammar2,Grammar3) :-
 	Grammar1=[Grammar4|Grammar5],
@@ -17,7 +56,14 @@ convert_to_grammar_part1(Grammar1,Grammar2,Grammar3) :-
 	Grammar6=[[n,grammar_part],Variables3,":-"],
 	convert_to_grammar_part2(Body1,1,3,2,[],Body2),
 	append(Grammar6,[Body2],Grammar7),
-	append(Grammar2,[Grammar7],Grammar8),
+	
+	%% member to check all doesn't work
+	((maplist(basecasecondition(Variables3,[n,Name]),Grammar2))->
+((Variables1=[]->(Grammar9=[[n,grammar_part],[[n,Name],[],[v,vgp]]],Grammar10=[[n,grammar_part],[[n,Name],"",[v,vgp]]]);fail %% need more for variable
+	),append(Grammar2,[Grammar9,
+	Grammar10,Grammar7],Grammar8));
+	append(Grammar2,[%%Grammar9,
+	Grammar7],Grammar8)),
 	convert_to_grammar_part1(Grammar5,Grammar8,Grammar3),!.
 convert_to_grammar_part1(Grammar1,Grammar2,Grammar3) :-
 	Grammar1=[Grammar4|Grammar5],
@@ -27,6 +73,9 @@ convert_to_grammar_part1(Grammar1,Grammar2,Grammar3) :-
 	Grammar4=[_Name4]),
 	append(Grammar2,[Grammar4],Grammar6),
 	convert_to_grammar_part1(Grammar5,Grammar6,Grammar3),!.
+	
+basecasecondition(Variables3,[n,Name],Item1) :-
+	not((Item1=[[n,grammar_part],Item2|_Rest2],Item2=[[n,Name]|_Rest3],length(Variables3,Length),length(Item2,Length))).
 	convert_to_grammar_part2([],_FirstVar,_SecondVar,_SecondVarParent,Body,Body) :- !.
 
 %% only want 2vp to be used if last call
@@ -157,12 +206,29 @@ call_grammar_part([[n,grammar_part]|_Variables]) :- !.
 call_or_terminal(Item) :-
 	terminal(Item)->true;call1(Item),!.
 	
+%% write grammar part
+
+%% [[]] in args
+
+%% collect code commands to write
+
+
+%% Terminal known, Phrase2 known
+%% Terminal unknown, Phrase2 known
+%% Terminal known, Phrase2 unknown
+%% Terminal unknown, Phrase2 unknown - error
+%% if known or unknown, can be [v]
+
+%% Gets val or sets prolog var to undef, puts val in undef in var
+
+%% Doesn't support [v], use wrap(v) x - append not string concat if P1 is ["a"]
+
 getvalues2([],Values,Values,_Vars,Flags,Flags) :- !.
 getvalues2(VarNames1,Values1,Values2,Vars,Flags1,Flags2) :-
 	VarNames1=[VarName1|VarNames2],
 	(VarName1=[VarName2]->Flag1=true;VarName2=VarName1),
 	getvalue(VarName2,Value1,Vars),
-	(isvar(Value1)->(true,Flag2=true);(Value2=Value1,Flag2=false)),
+	(Value1=empty->Flag2=true;(Value2=Value1,Flag2=false)),
 	(Flag1=true->Value3=[Value2];Value3=Value2),
 	append(Values1,Value3,Values3),
 	append(Flags1,[Flag2],Flags3),
