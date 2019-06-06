13b01e220edb13b78b16e6df69c9ab347a5f5135
Lucian Green <luciangreen@lucianacademy.com>
2019-06-06 20:19:21 +1000

    Add files via upload

--- mindreadtestpsychiatrist.pl
+++ mindreadtestpsychiatrist.pl
@@ -2,6 +2,63 @@
 
 %% Make files different for different tests
 
+/**
+1. Breason out 250 br A - as receiver/idea	
+2. Time this for A for the topic breasoned out and not breasoned out at random intervals for an interval of time (not to do with the random intervals), person to me
+3. Does it take considerably longer for detected As? - calculate median of mostly hopefully false trials and differences from it x labelled graph first - do 10 trials over 10 minutes with 3 hopefully successful attempts that are labelled
+
+[dateandtime,date(2019,4,27,10,28,25.09528613090515,0,UTC,false)]
+[[*5,lorelle],[5,dam],[5,adrian],[4,water],[4,seed],[2,redblackduck]]
+lorelle but after start
+
+[dateandtime,date(2019,4,27,10,28,50.22005605697632,0,UTC,false)]
+[[8,seed],[6,redblackduck],[*4,dam],[3,water],[3,adrian],[1,lorelle]]
+dam
+
+[dateandtime,date(2019,4,27,10,29,13.600827693939209,0,UTC,false)]
+[[*7,redblackduck],[7,lorelle],[6,dam],[5,seed],[3,water],[3,adrian]]
+red black duck after start
+
+[dateandtime,date(2019,4,27,10,29,37.128156661987305,0,UTC,false)]
+[[6,seed],[6,dam],[5,water],[*5,redblackduck],[4,adrian],[3,lorelle]]
+red black duck
+
+[dateandtime,date(2019,4,27,10,30,1.5190646648406982,0,UTC,false)]
+[[9,adrian],[*8,redblackduck],[7,dam],[6,seed],[5,water],[4,lorelle]]
+red black duck
+
+Is it detecting what I am really thinking (or someone else) are the thoughts intertwined - all x
+
+I seem to have really thought about 1. the main character 2. the reason 3. the next character 4. what was later 5. who would put them away
+
+Is the seen as version able to be used?  Is it a stream of consciousness detector?
+
+***250s for dotting yourself on speaking and dotting yourself receiving, and yourself speaking and yourself receiving (each day?)
+
+
+[dateandtime,date(2019,4,27,11,5,29.561346292495728,0,UTC,false)]
+[[7,seed],[7,adrian],[*6,water],[6,lorelle],[6,dam],[4,redblackduck]]
+water
+[dateandtime,date(2019,4,27,11,5,53.5652551651001,0,UTC,false)]
+[[7,seed],[*6,water],[6,dam],[5,lorelle],[3,redblackduck],[2,adrian]]
+water
+
+[dateandtime,date(2019,4,27,11,6,17.240721464157104,0,UTC,false)]
+[[*6,dam],[5,redblackduck],[5,adrian],[4,water],[4,seed],[3,lorelle]]
+dam
+
+[dateandtime,date(2019,4,27,11,6,41.15016746520996,0,UTC,false)]
+[[7,redblackduck],[7,lorelle],[*7,dam],[6,adrian],[3,water],[3,seed]]
+dam
+
+*** think to binary opposition x objects to either side - just do 250s for l,r, then turn off with a 250
+
+*** put back positive protective thoughts on top, just do 250, then turn off with 250
+
+working ok
+
+**/
+
 %%use_module(library(pio)).
 
 :- use_module(library(date)).
