98b3208123d6eabb86c981c5a5095bc6fc3bd067
Lucian Green <luciangreen@lucianacademy.com>
2019-09-13 22:52:53 +1000

    Create meditatorsanddoctors.pl

---
+++ meditatorsanddoctors.pl
@@ -0,0 +1,13 @@
+%% meditatorsanddoctors.pl
+
+%% Name, DOB, Date learned, psych appointment month=1 or 2, psych appointment day, thoughts count
+
+meditators([
+%%/**
+[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16], %% 
+[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16] %% 
+]).
+
+%% empty, because meditators is now based on 50 As
+doctors([
+]).
