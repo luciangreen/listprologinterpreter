01054d59b54944671896223852660851b5c229d3
Lucian Green <luciangreen@lucianacademy.com>
2021-03-15 21:27:56 +1100

    la_files.pl

---
+++ la_files.pl
@@ -0,0 +1,2 @@
+delete_invisibles_etc(F,G) :-
+	findall(J,(member(H,F),atom_string(H,J),not(J="."),not(J=".."),not(string_concat(".",_,J))),G).
--- la_strings_string.pl
+++ la_strings_string.pl
@@ -2,3 +2,4 @@ string(String) --> list(String).
 
 list([]) --> [].
 list([L|Ls]) --> [L], list(Ls).
+
