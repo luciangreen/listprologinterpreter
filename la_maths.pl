349427c1c2cf8574ce37abc4ed32ab00e62147de
Lucian Green <luciangreen@lucianacademy.com>
2021-11-28 14:26:52 +1100

    Update la_maths.pl

--- la_maths.pl
+++ la_maths.pl
@@ -3,8 +3,8 @@
 %% numbers(5,1,[],N).
 %% N=[1,2,3,4,5]
 
-numbers(N2,N1,Numbers2):-
-	(N2>N1->
+numbers(N2,N1,Numbers2) :-
+	(N2>=N1->
 	numbers(N2,N1,[],Numbers2);
 	numbers(N1,N2,[],Numbers2)).
 
