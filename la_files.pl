b227faeaa58ece203e9dae6ec1b1b6baa5718f1a
Lucian Green <luciangreen@lucianacademy.com>
2022-07-02 13:51:53 +1000

    Update la_files.pl

--- la_files.pl
+++ la_files.pl
@@ -1,10 +1,18 @@
 delete_invisibles_etc(F,G) :-
 	findall(J,(member(H,F),atom_string(H,J),not(J="."),not(J=".."),not(string_concat(".",_,J))),G),!.
 
+
 open_file_s(Path,File_term) :-
 		phrase_from_file_s(string(File), Path),
 		string_codes(File_string,File),
 		term_to_atom(File_term,File_string),!.
+
+
+open_string_file_s(Path,File_string) :-
+		phrase_from_file_s(string(File), Path),
+		string_codes(File_string,File),!.
+		%term_to_atom(File_term,File_string),!.
+
 
 save_file_s(Path,Content_term_or_string) :-
 	((compound(Content_term_or_string)->true;Content_term_or_string=[])->term_to_atom(Content_term_or_string,String);(string(Content_term_or_string)->Content_term_or_string=String;
