3b22c896ce6bdfaa13020ff5efc4c34c25e53f6d
Lucian Green <luciangreen@lucianacademy.com>
2023-05-23 22:06:04 +1000

    Update la_files.pl

--- la_files.pl
+++ la_files.pl
@@ -15,8 +15,8 @@ open_string_file_s(Path,File_string) :-
 
 
 save_file_s(Path,Content_term_or_string) :-
-	((compound(Content_term_or_string)->true;Content_term_or_string=[])->term_to_atom(Content_term_or_string,String);(string(Content_term_or_string)->Content_term_or_string=String;
-	(concat_list(["Error: save_file_s content not in compound or string format."],Notification),
+	((compound(Content_term_or_string)->true;Content_term_or_string=[])->term_to_atom(Content_term_or_string,String);((string(Content_term_or_string)->true;atom(Content_term_or_string))->Content_term_or_string=String;
+	(concat_list(["Error: save_file_s content not in compound, atom or string format."],Notification),
 	writeln0(Notification),abort))),
 	(open_s(Path,write,Stream),
 	write(Stream,String),
